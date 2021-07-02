fd91666c5ff06aaae74d3ac0954a061b917b4f37
NSklenicka <75758536+NSklenicka@users.noreply.github.com>
2021-07-02 08:44:36 -0400

    know good read and write

--- BCM_I2C_TEST/main.cpp
+++ BCM_I2C_TEST/main.cpp
@@ -3,12 +3,63 @@
 #include <qdebug.h>
 #include <iostream>
 
-//#define REGISTER 0x20
-//#define DATA 0xAB
-#define PRINT_CHAR(ch) std::cout << std::hex << (0xFF & ch) << std::endl
 
+void printByte(char address)//reads a byte at an address and print to console
+{
+    char buf[] = {address}; //0x20 0xAB
+    int result;
 
-//this code works!
+    //send the data address to be read
+    result = bcm2835_i2c_write (buf,1);
+
+    if(0 == result)
+    {
+        //success
+        qDebug() << "printByte : write success";
+    }
+    else
+    {
+        //failed
+        qDebug() << "printByte : write failed"; //always NACKS but actually worked??
+        qDebug() << "result: " << result;
+    }
+
+    //read the data at the address
+    result = bcm2835_i2c_read (buf,1);
+    if(0 == result)
+    {
+        //success
+        qDebug() << "printByte : read success";
+        printf("address:    0x%02X \r\n", address);
+        printf("data:       0x%02X \r\n", buf[0]); //big X means print the value in capital HEX notation
+    }                                               //02 means print 2 digits left padded by 0's
+    else
+    {
+        //failed
+        qDebug() << "printByte : read failed";
+        qDebug() << "result: " << result;
+    }
+}
+
+void writeByte(char address, char data)
+{
+    char buf[] = {address, data, 0};
+
+    int result = bcm2835_i2c_write (buf, 3);
+
+    if(0 == result)
+    {
+        //success
+        qDebug() << "writeByte : write success";
+    }
+    else
+    {
+        //failed
+        qDebug() << "writeByte : write failed"; //always NACKS but actually worked??
+        qDebug() << "result: " << result;
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -32,71 +83,36 @@ int main(int argc, char *argv[])
 
     char address = 0x01;
     char data1 = 0x52;
-
-    char buf[] = {address}; //0x20 0xAB
-
-    int result;
-
-    //write data
-    //send the address followed by the data
+    char data2 = 0x63;
 
 
+    //write data1 to address
+    //qDebug() << "writing " << data1 << " to address " << address;
+    writeByte(address, data1);
 
+    bcm2835_delay(500);
 
-    //read data
-    //send the data address to be read
+    //read data 1 from address
+    printByte(address);
 
-    //buf[0] = {REGISTER}; //0X20
+    bcm2835_delay(500);
 
-    result = bcm2835_i2c_write (buf,1);
+    //write data2 to address
+    //qDebug() << "writing " << data2 << " to address " << address;
+    writeByte(address, data2);
 
-    if(0 == result)
-    {
-        //success
-        qDebug() << "write success";
-    }
-    else
-    {
-        //failed
-        qDebug() << "write failed"; //always NACKS but actually worked??
-        qDebug() << "result: " << result;
-    }
+    bcm2835_delay(500);
 
-    //clear the buffer (for debugging)
-    buf[0] = 0;
-    //read the data at the address
-    result = bcm2835_i2c_read (buf,1);
+    //read data2 from address
+    printByte(address);
 
-    if(0 == result)
-    {
-        //success
-        qDebug() << "read success";
-        printf("User Register = 0x%02X \r\n",buf[0]); //big X means print the value in capital HEX notation
-    }                                               //02 means print 2 digits left padded by 0's
-    else
-    {
-        //failed
-        qDebug() << "read failed";
-        qDebug() << "result: " << result;
-    }
-
-    //read the data at the next address
-
-    if(0 == bcm2835_i2c_read (buf,1))
-    {
-        //success
-        qDebug() << "read success";
-        printf("User Register = 0x%02X \r\n",buf[0]);
-    }
-    else
-    {
-        //failed
-        qDebug() << "read failed";
-    }
+    bcm2835_delay(500);
 
 
     bcm2835_i2c_end();
     bcm2835_close();
+
+    qDebug() << "done";
 
     return a.exec();
 }
