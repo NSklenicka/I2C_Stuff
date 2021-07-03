c0d0170422337758088a71dbdf8274fe212b0594
NSklenicka <75758536+NSklenicka@users.noreply.github.com>
2021-07-03 07:06:17 -0400

    test for minumum delay between commands

--- BCM_I2C_TEST/main.cpp
+++ BCM_I2C_TEST/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 
-void printByte(char address)//reads a byte at an address and print to console
+bool printByte(char address)//reads a byte at an address and print to console
 {
     char buf[] = {address}; //0x20 0xAB
     int result;
@@ -22,6 +22,7 @@ void printByte(char address)//reads a byte at an address and print to console
         //failed
         qDebug() << "printByte : write failed"; //always NACKS but actually worked??
         qDebug() << "result: " << result;
+        return false;
     }
 
     //read the data at the address
@@ -32,16 +33,19 @@ void printByte(char address)//reads a byte at an address and print to console
         qDebug() << "printByte : read success";
         printf("address:    0x%02X \r\n", address);
         printf("data:       0x%02X \r\n", buf[0]); //big X means print the value in capital HEX notation
+        return true;
     }                                               //02 means print 2 digits left padded by 0's
+
     else
     {
         //failed
         qDebug() << "printByte : read failed";
         qDebug() << "result: " << result;
+        return false;
     }
 }
 
-void writeByte(char address, char data)
+bool writeByte(char address, char data)
 {
     char buf[] = {address, data, 0};
 
@@ -51,12 +55,14 @@ void writeByte(char address, char data)
     {
         //success
         qDebug() << "writeByte : write success";
+        return true;
     }
     else
     {
         //failed
         qDebug() << "writeByte : write failed"; //always NACKS but actually worked??
         qDebug() << "result: " << result;
+        return false;
     }
 }
 
@@ -84,33 +90,71 @@ int main(int argc, char *argv[])
     char address = 0x01;
     char data1 = 0x52;
     char data2 = 0x63;
+    int delayMS = 10;
+
+    //testing to see minimum delay
+
+    //delay MS needs to be at least 3 ms
+
+//    while((delayMS > 0))
+//    {
+//        qDebug() << "delay = " << delayMS;
+
+//        if(!writeByte(address, data1))
+//            break;
+
+//        bcm2835_delay(delayMS);
+
+//        //read data 1 from address
+//        if(!printByte(address))
+//            break;
+
+//        bcm2835_delay(delayMS);
+
+//        if(!writeByte(address, data2))
+//            break;
+
+//        bcm2835_delay(delayMS);
+
+//        if(!printByte(address))
+//            break;
+
+//        bcm2835_delay(delayMS);
+
+//        delayMS = delayMS - 1;
+//    }
+//    qDebug() << "final delay ms = " << delayMS;
 
 
-    //write data1 to address
-    //qDebug() << "writing " << data1 << " to address " << address;
-    writeByte(address, data1);
-
-    bcm2835_delay(500);
-
-    //read data 1 from address
-    printByte(address);
-
-    bcm2835_delay(500);
-
-    //write data2 to address
-    //qDebug() << "writing " << data2 << " to address " << address;
-    writeByte(address, data2);
-
-    bcm2835_delay(500);
-
-    //read data2 from address
-    printByte(address);
-
-    bcm2835_delay(500);
 
 
-    bcm2835_i2c_end();
-    bcm2835_close();
+
+
+
+//    //write data1 to address
+//    writeByte(address, data1);
+
+//    bcm2835_delay(500);
+
+//    //read data 1 from address
+//    printByte(address);
+
+//    bcm2835_delay(500);
+
+//    //write data2 to address
+//    //qDebug() << "writing " << data2 << " to address " << address;
+//    writeByte(address, data2);
+
+//    bcm2835_delay(500);
+
+//    //read data2 from address
+//    printByte(address);
+
+//    bcm2835_delay(500);
+
+
+//    bcm2835_i2c_end();
+//    bcm2835_close();
 
     qDebug() << "done";
 
