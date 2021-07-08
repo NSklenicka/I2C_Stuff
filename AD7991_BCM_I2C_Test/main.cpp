d4ffa84d9486db76bb3e3ebb1750ed65cdb86016
NSklenicka <75758536+NSklenicka@users.noreply.github.com>
2021-07-08 10:33:14 -0400

    working

--- AD7991_BCM_I2C_Test/main.cpp
+++ AD7991_BCM_I2C_Test/main.cpp
@@ -2,6 +2,7 @@
 #include <bcm2835.h>
 #include <qdebug.h>
 #include <iostream>
+#include <bitset>
 
 int main(int argc, char *argv[])
 {
@@ -31,10 +32,25 @@ int main(int argc, char *argv[])
 
     int result = 99;
 
+    //select channels
+    std::bitset<8> channels = {0};
+
+    bool ch0 = 1;
+    bool ch1 = 0;
+    bool ch2 = 0;
+    bool ch3 = 0;
+
+    if(ch0) channels[4] = 1;
+    if(ch1) channels[5] = 1;
+    if(ch2) channels[6] = 1;
+    if(ch3) channels[7] = 1;
+
+    char config = channels.to_ulong();
+
+    qDebug() << "channels: " << QString::number(config, 2);
+
     //write to configuration register
-
-
-    buf[0] = {0x20};
+    buf[0] = {config};
 
     result = bcm2835_i2c_write(buf, 1);
     if(0 == result)
@@ -72,34 +88,43 @@ int main(int argc, char *argv[])
         //return false;
     }
 
-//    bcm2835_delay(20);
+////////////////    parse out the data response   ///////////////////////////////
+
+    char channel;
+
+    //get channel
+    channel = ((data[0] & 0b00110000)>>4); //mask the 2 channel bits, shift them to the right
+    qDebug() << "channel: " << static_cast<int>(channel);
+
+    //do I need static cast??
+//    int ch = ((buf[0] & 0b00110000)>>4);
+//    qDebug() << "channel not static cast: " << ch;
+
+    //get value
+    int data0, data1;
+    float datan;
+    data0 = static_cast<int>(data[0] & 0b00001111); //mask out the channel bits on buf[0]
+    //qDebug() << "data0 after mask: " << QString::number(data0, 2);
+
+    data0 = (data0 << 8); // bit shift 8 bits to the left
+    //qDebug() << "data0 after << 8 : " << QString::number(data0, 2);
+
+    data1 = data[1] & 0xFF;//0xFF will over flow ??
+    //qDebug() << "data1: "  << QString::number(data1, 2);
+
+    datan = data0 + data1;
+
+    qDebug() << "data0 + data1: " << QString::number(datan);
+
+    float Vref = 3.294;
+    float value = datan/4095;
+    qDebug() << "data/4095: " << value;
+
+    value = value * Vref;
+
+    qDebug() << "value: " << value;
 
 
-//    //read 2 bytes from A/D
-
-//    data[0] = 0;
-//    data[0] = 1;
-//    result = bcm2835_i2c_read(data, 2);
-//    if(0 == result)
-//    {
-//        //success
-//        qDebug() << "i2c_read success";
-//        printf("data[0] : 0x%02X \r\n", data[0]);
-//        printf("data[1] : 0x%02X \r\n", data[1]);
-//    }
-//    else
-//    {
-//        //failed
-//        qDebug() << "i2c_read failed";
-//        qDebug() << "result: " << result;
-//        //return false;
-//    }
-
-
-
-//    bcm2835_delay(20);
-
-    ////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //    bcm2835_i2c_end();
